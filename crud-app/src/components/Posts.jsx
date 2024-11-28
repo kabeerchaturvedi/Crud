84c41fd70f8bfd04d8c85dd1046be51f2ff678d2
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2024-11-28 13:09:16 +0530

    Added tailwind and fetch

--- crud-app/src/components/Posts.jsx
+++ crud-app/src/components/Posts.jsx
@@ -1,7 +1,39 @@
-
+import { useEffect, useState } from "react";
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchPostsData();
+  }, []);
+
+  const fetchPostsData = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await data.json();
+    setData(response);
+  };
+
+  return (
+    <div className="flex justify-center p-10">
+      <table className="border-collapse border border-slate-400">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((post, index) => (
+            <tr key={index}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>{post.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Posts;
