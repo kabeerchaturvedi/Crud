d8e08cfebb707b824bdd8f809095cda22263cabc
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2024-12-04 19:27:39 +0530

    Delete done

--- crud-app/src/components/Posts/Posts.jsx
+++ crud-app/src/components/Posts/Posts.jsx
@@ -15,6 +15,21 @@ const Posts = () => {
     setData(response);
   };
 
+  const deletePosts = async (post) => {
+    console.log(post);
+    try {
+      const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${post}`, {
+        method: "DELETE",
+      });
+      if (data) {
+        alert("Data Deleted");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    fetchPostsData();
+  };
+
   return (
     <div>
       <div className="mb-4">
@@ -47,7 +62,10 @@ const Posts = () => {
                   <button className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
                     Edit
                   </button>
-                  <button className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+                  <button
+                    className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                    onClick={() => deletePosts(post.id)}
+                  >
                     Delete
                   </button>
                 </div>
